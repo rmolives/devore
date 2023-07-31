088e39feb130b086df6a94e829798b3e66c94b2e
RMOlive <rmolives@wumoe.org>
2023-08-01 02:18:09 +0800

    script

--- src/org/wumoe/devore/Main.java
+++ src/org/wumoe/devore/Main.java
@@ -1,19 +1,13 @@
 package org.wumoe.devore;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+import org.wumoe.devore.lang.Env;
+
 import javax.script.ScriptException;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ScriptException {
-        // Repl.repl(System.in, System.out, Env.newEnv().load("core"));
-
-        ScriptEngineManager manager = new ScriptEngineManager(ClassLoader.getSystemClassLoader());
-
-        ScriptEngine engine = manager.getEngineByName("devore");
-
-        System.out.println(engine.eval("(+ 2 3)"));
+    public static void main(String[] args) throws IOException {
+        Repl.repl(System.in, System.out, Env.newEnv().load("core"));
     }
 }
