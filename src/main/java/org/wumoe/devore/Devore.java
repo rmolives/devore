7e2e1025bc94d8e64dae74c09cb263273f6fdc6e
RMOlive <rmolives@wumoe.org>
2023-08-04 06:55:22 +0800

    alpha

--- src/main/java/org/wumoe/devore/Devore.java
+++ src/main/java/org/wumoe/devore/Devore.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Devore {
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.1-alpha";
     public static Map<String, Module> moduleTable = new HashMap<>() {
         {
             put("core", new Core());
