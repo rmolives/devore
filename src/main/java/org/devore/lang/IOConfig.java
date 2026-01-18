49455e15b7fc574b399fc6818c6d5dc239cf6e59
RMOlive <rmolives@wumoe.org>
2026-01-18 23:14:56 +0800

    订正

--- src/main/java/org/devore/lang/IOConfig.java
+++ src/main/java/org/devore/lang/IOConfig.java
@@ -3,13 +3,15 @@ package org.devore.lang;
 import java.io.InputStream;
 import java.io.PrintStream;
 
-
+/**
+ * IO表
+ */
 public final class IOConfig {
     public final InputStream in;    // 输入流
     public final PrintStream out;   // 输出流
 
     /**
-     * IO表
+     * 创建IO表
      *
      * @param out 输出流
      * @param in  输入流
