d2088f328dd8df36bad72d288cac2d895e43ba97
RMOlive <rmolives@wumoe.org>
2026-01-19 15:24:34 +0800

    repl

--- src/main/java/org/devore/Repl.java
+++ src/main/java/org/devore/Repl.java
@@ -10,15 +10,14 @@ public class Repl {
     /**
      * REPL
      *
-     * @param dEnv 环境
+     * @param env 环境
      * @throws IOException 错误
      */
-    public static void repl(Env dEnv) throws IOException {
-        InputStream in = dEnv.io.in;
-        PrintStream out = dEnv.io.out;
+    public static void repl(Env env) throws IOException {
+        InputStream in = env.io.in;
+        PrintStream out = env.io.out;
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
         StringBuilder codeBuilder = new StringBuilder();
-        Env env = dEnv;
         int size = 0;
         while (true) {
             out.print("[Devore] >>> ");
@@ -29,43 +28,35 @@ public class Repl {
             int index = 0;
             int flag = 0;
             String read = reader.readLine();
-            if (read.startsWith(":exit"))
-                break;
-            else if (read.startsWith(":version"))
-                out.println(Devore.VERSION_MESSAGE);
-            else if (read.startsWith(":clear"))
-                env = Env.newEnv();
-            else {
-                codeBuilder.append(read);
-                char[] codeCharArray = codeBuilder.toString().toCharArray();
-                boolean skip = false;
-                while (codeCharArray[index] != '(' && codeCharArray[index] != '[')
+            codeBuilder.append(read);
+            char[] codeCharArray = codeBuilder.toString().toCharArray();
+            boolean skip = false;
+            while (codeCharArray[index] != '(' && codeCharArray[index] != '[')
+                ++index;
+            do {
+                if (index < codeCharArray.length - 1 && codeCharArray[index] == '\\') {
+                    skip = true;
                     ++index;
-                do {
-                    if (index < codeCharArray.length - 1 && codeCharArray[index] == '\\') {
-                        skip = true;
-                        ++index;
-                        continue;
-                    }
-                    if (codeCharArray[index] == '\"') {
-                        do {
-                            if (skip) {
-                                skip = false;
-                                ++index;
-                                continue;
-                            }
+                    continue;
+                }
+                if (codeCharArray[index] == '\"') {
+                    do {
+                        if (skip) {
+                            skip = false;
                             ++index;
-                            if (codeCharArray[index] == '\"')
-                                break;
-                        } while (index < codeCharArray.length - 1);
-                    }
-                    if (codeCharArray[index] == '(' || codeCharArray[index] == '[')
-                        ++flag;
-                    else if (codeCharArray[index] == ')' || codeCharArray[index] == ']')
-                        --flag;
-                    ++index;
-                } while (index < codeCharArray.length);
-            }
+                            continue;
+                        }
+                        ++index;
+                        if (codeCharArray[index] == '\"')
+                            break;
+                    } while (index < codeCharArray.length - 1);
+                }
+                if (codeCharArray[index] == '(' || codeCharArray[index] == '[')
+                    ++flag;
+                else if (codeCharArray[index] == ')' || codeCharArray[index] == ']')
+                    --flag;
+                ++index;
+            } while (index < codeCharArray.length);
             if (flag == 0) {
                 Token result = Devore.call(env, codeBuilder.toString());
                 codeBuilder = new StringBuilder();
