1cc236e8f9b2de792f79901038c9d62e901ad28c
RMOlive <rmolives@wumoe.org>
2026-01-20 19:56:15 +0800

    自动适应相对路径和绝对路径

--- src/main/java/org/devore/Main.java
+++ src/main/java/org/devore/Main.java
@@ -2,7 +2,6 @@ package org.devore;
 
 import org.devore.lang.Env;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -13,7 +12,7 @@ public class Main {
         if (args.length == 0) Repl.repl(Env.newEnv());
         else if ("--version".equals(args[0]) || "-v".equals(args[0])) System.out.println(Devore.VERSION_MESSAGE);
         else Devore.call(Env.newEnv(), new String(Files.readAllBytes(
-                            Paths.get(System.getProperty("user.dir") + File.separator + args[0])),
-                            StandardCharsets.UTF_8));
+                    Paths.get(System.getProperty("user.dir")).resolve(Paths.get(args[0])).normalize()),
+                    StandardCharsets.UTF_8));
     }
 }
