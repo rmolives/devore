627cb4dd7b8f565d4e287d58482e7d98e3fea6e2
RMOlive <rmolives@wumoe.org>
2026-06-23 19:40:52 +0800

    FormatUtils字符串转译

--- src/main/java/org/devore/utils/FormatUtils.java
+++ src/main/java/org/devore/utils/FormatUtils.java
@@ -6,7 +6,40 @@ import org.devore.lang.token.DToken;
 public class FormatUtils {
     public static String formatToken(DToken token) {
         if (token instanceof DString)
-            return "\"" + token + "\"";
+            return "\"" + escapeString(token.toString()) + "\"";
         return token.toString();
+    }
+
+    private static String escapeString(String str) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < str.length(); ++i) {
+            switch (str.charAt(i)) {
+                case '\\':
+                    builder.append("\\\\");
+                    break;
+                case '"':
+                    builder.append("\\\"");
+                    break;
+                case '\n':
+                    builder.append("\\n");
+                    break;
+                case '\r':
+                    builder.append("\\r");
+                    break;
+                case '\t':
+                    builder.append("\\t");
+                    break;
+                case '\b':
+                    builder.append("\\b");
+                    break;
+                case '\f':
+                    builder.append("\\f");
+                    break;
+                default:
+                    builder.append(str.charAt(i));
+                    break;
+            }
+        }
+        return builder.toString();
     }
 }
