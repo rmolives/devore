3cd426b3bca3e2e59e51b214733017a76423bd52
RMOlive <rmolives@wumoe.org>
2026-06-23 21:01:48 +0800

    escapeString

--- src/main/java/org/devore/utils/FormatUtils.java
+++ src/main/java/org/devore/utils/FormatUtils.java
@@ -3,6 +3,8 @@ package org.devore.utils;
 import org.devore.lang.token.DString;
 import org.devore.lang.token.DToken;
 
+import java.util.stream.Collectors;
+
 public class FormatUtils {
     public static String formatToken(DToken token) {
         if (token instanceof DString)
@@ -11,35 +13,27 @@ public class FormatUtils {
     }
 
     private static String escapeString(String str) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < str.length(); ++i) {
-            switch (str.charAt(i)) {
-                case '\\':
-                    builder.append("\\\\");
-                    break;
-                case '"':
-                    builder.append("\\\"");
-                    break;
-                case '\n':
-                    builder.append("\\n");
-                    break;
-                case '\r':
-                    builder.append("\\r");
-                    break;
-                case '\t':
-                    builder.append("\\t");
-                    break;
-                case '\b':
-                    builder.append("\\b");
-                    break;
-                case '\f':
-                    builder.append("\\f");
-                    break;
-                default:
-                    builder.append(str.charAt(i));
-                    break;
-            }
-        }
-        return builder.toString();
+        return str.chars()
+                .mapToObj(c -> {
+                    switch (c) {
+                        case '\\':
+                            return "\\\\";
+                        case '"':
+                            return "\\\"";
+                        case '\n':
+                            return "\\n";
+                        case '\r':
+                            return "\\r";
+                        case '\t':
+                            return "\\t";
+                        case '\b':
+                            return "\\b";
+                        case '\f':
+                            return "\\f";
+                        default:
+                            return String.valueOf((char) c);
+                    }
+                })
+                .collect(Collectors.joining());
     }
 }
